d90768b7419b3c7a4ae305b02b0068d4abefa184
Harold <haroldkaluba8@gmail.com>
2025-05-29 14:03:46 +0300

    Removed valiables and replaced them wit string

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -5,13 +5,13 @@ cold = "Make sure to wear a warm coat and a scarf."
 weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
 if weather == "sunny": 
-    print(sunny)
+    print("Wear a t-shirt and sunglasses.")
 
 elif weather == "rainy":
-    print(rainy)
+    print("Don't forget your umbrella and a raincoat.")
 
 elif weather == "cold":
-    print(cold)
+    print("Make sure to wear a warm coat and a scarf.")
 
 else:
     print("Sorry, I don't have recommendations for this weather.")
