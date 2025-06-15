02535ff450dd8b07aa283b80fd83bebb92ec885c
Harold <haroldkaluba8@gmail.com>
2025-06-15 17:53:24 +0300

    Add library manager

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,61 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False  # Already checked out
+
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False  # Not checked out
+
+    def is_available(self):
+        return not self._is_checked_out
+
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self._books.append(book)
+        else:
+            print("Can only add Book objects to the library.")
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                if book.check_out():
+                    print(f"'{title}' has been checked out.")
+                else:
+                    print(f"'{title}' is already checked out.")
+                return
+        print(f"'{title}' not found in the library.")
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                if book.return_book():
+                    print(f"'{title}' has been returned.")
+                else:
+                    print(f"'{title}' was not checked out.")
+                return
+        print(f"'{title}' not found in the library.")
+
+    def list_available_books(self):
+        found_available = False
+        for book in self._books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
+                found_available = True
+        if not found_available and not self._books:
+            print("No books in the library.")
+        elif not found_available:
+            print("No books currently available.")
